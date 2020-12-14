29f2a3252252f66e8cb14aa492d676cb892c46de
Harsh Pawar <31727764+harshp21@users.noreply.github.com>
2020-12-14 16:05:04 +0530

    Update players.ts

--- Cricket-game/players.ts
+++ Cricket-game/players.ts
@@ -10,9 +10,39 @@ export class Player {
     private totalScore: number = 0;
     private name: string;
     private runs: Array<ScorePerBall> = [];
+    private domElement: HTMLElement;
+    private id: number;
+    private noOfBallsToFace: number = 6;
+    private teamId: number;
 
-    constructor(name: string) {
+    constructor(name: string, id: number, teamId: number) {
         this.name = name;
+        this.id = id;
+        this.teamId = teamId;
+        this.domElement = this.createDomElement();
+
+    }
+
+    private createDomElement(): HTMLElement {
+        let scoreboardPlayerRow = document.createElement('tr');
+        let scoreBoardPlayerData = document.createElement('td');
+        scoreBoardPlayerData.innerHTML = this.name;
+        scoreboardPlayerRow.appendChild(scoreBoardPlayerData);
+
+        for (let ball = 1; ball <= this.noOfBallsToFace; ball++) {
+            let scoreBoardPlayerData = document.createElement('td');
+            scoreBoardPlayerData.id = `T${this.teamId}B${ball}P${this.id}`;
+            scoreboardPlayerRow.appendChild(scoreBoardPlayerData);
+        }
+
+        let scoreboardPlayerTotal = document.createElement('td');
+        scoreboardPlayerTotal.id = `T${this.teamId}P${this.id}-total`;
+        scoreboardPlayerRow.appendChild(scoreboardPlayerTotal);
+        return scoreboardPlayerRow;
+    }
+
+    public getDomElement(): HTMLElement {
+        return this.domElement;
     }
 
     public getTotalScore(): number {
@@ -30,6 +60,16 @@ export class Player {
 
     public getPlayerName(): string {
         return this.name;
+    }
+
+    public hitRun(team: Team, ballNo: number, runs: number): void {
+        let runScored = {
+            ballNo: ballNo,
+            runScored: runs,
+        }
+        this.setRuns(runScored);
+        (<HTMLInputElement>document.getElementById(`T${this.teamId}B${ballNo}P${this.id}`)).innerHTML = `${runs}`;
+        (<HTMLInputElement>document.getElementById(`T${this.teamId}-score`)).innerHTML = `${team.calculateTotalTeamScore()}`;
     }
 
 }
